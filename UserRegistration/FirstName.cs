d6a9eac607e67c78c08dfdbc0398eddb29a4adfe
Rak65 <rakeshp6505@gmail.com>
2023-05-26 11:01:51 +0530

    As a user need to enter a valid First Name

---
+++ UserRegistration/FirstName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class FirstName
+    {
+        public static bool IsValidFirstName(string firstName)
+        {
+            // Regular expression pattern for first name validation
+            string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+
+            // Check if the first name matches the pattern
+            return Regex.IsMatch(firstName, pattern);
+        }
+    }
+}
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -14,7 +14,7 @@ namespace UserRegistration
             Console.WriteLine("Enter your first name: ");
             string firstName = Console.ReadLine();
 
-            if (IsValidFirstName(firstName))
+            if (FirstName.IsValidFirstName(firstName))
             {
                 Console.WriteLine("Valid first name.");
             }
@@ -23,14 +23,6 @@ namespace UserRegistration
                 Console.WriteLine("Invalid first name.");
             }
             Console.ReadKey();
-        }
-        static bool IsValidFirstName(string firstName)
-        {
-            // Regular expression pattern for first name validation
-            string pattern = @"^[A-Z][a-zA-Z]{2,}$";
-
-            // Check if the first name matches the pattern
-            return Regex.IsMatch(firstName, pattern);
         }
     }
 }
